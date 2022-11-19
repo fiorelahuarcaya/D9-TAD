247ccdd49ae8e7e62d1ddd5353b924f8e6d082f6
Jhonatan <jhonatan.guevara@unmsm.edu.pe>
2022-11-19 17:12:50 -0500

    Memento

--- example/memento/MementoExample.js
+++ example/memento/MementoExample.js
@@ -1,52 +1,51 @@
+import React from "react";
 
-import React from 'react'
-
+// Contador: Clic -> Aumenta en uno ; restore -> memento
 let Counter = (props, ref) => {
-  const [ count, setCount ] = React.useState(0)
+  const [count, setCount] = React.useState(0);
 
   React.useImperativeHandle(ref, () => ({
     createMemento: () => {
-      console.log('Created memento with count ' + count)
-      return { count }
+      console.log("Created memento with count " + count);
+      return { count };
     },
     restore: (memento) => {
-      console.log('Restored memento')
-      setCount(memento.count)
+      console.log("Restored memento");
+      setCount(memento.count);
     },
-  }))
+  }));
 
   return (
     <div
       children={count}
-      onClick={() => setCount(prevCount => prevCount + 1)}
-      style={{ fontSize: '32px', userSelect: 'none' }}
+      onClick={() => setCount((prevCount) => prevCount + 1)}
+      style={{ fontSize: "32px", userSelect: "none" }}
     />
-  )
-}
+  );
+};
 
-Counter = React.forwardRef(Counter)
+Counter = React.forwardRef(Counter);
 
+// Objeto cliente que crea el contador y los mementos.
 const Client = () => {
+  const ref = new React.createRef();
 
-  const ref = new React.createRef()
-
-  const [ memento, setMemento ] = React.useState()
+  const [memento, setMemento] = React.useState();
 
   return (
     <React.Fragment>
       <Counter ref={ref} />
 
-      <button 
+      <button
         onClick={() => setMemento(ref.current.createMemento())}
         children="Create memento"
       />
-      <button 
+      <button
         onClick={() => ref.current.restore(memento)}
         children="Restore memento"
       />
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Client
-
+export default Client;
