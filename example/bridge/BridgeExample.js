decd3bafb18fda1b3cbac5bc8e699755ee3e6661
Jhonatan <jhonatan.guevara@unmsm.edu.pe>
2022-11-19 15:11:34 -0500

    Comentarios

--- example/bridge/BridgeExample.js
+++ example/bridge/BridgeExample.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+// Configuración del cómo se ve el botón
 const ButtonUI = ({ theme, ...props }) => {
   return (
     <button
@@ -11,16 +12,18 @@ const ButtonUI = ({ theme, ...props }) => {
     />
   );
 };
-// BURRA EL QUE LO ENCUENTRE
+
+// Funcionalidad del botón
 const Link = ({ url, uiComponent, uiProps, children }) => {
   const bridgeProps = {
     ...uiProps,
-    onClick: () => window.open(url, "_blank"),
+    onClick: () => window.open(url, "_blanket"),
   };
 
   return React.createElement(uiComponent, bridgeProps, children);
 };
 
+// Usuario Cliente que accede a dicho recurso
 const Client = () => {
   const theme = { backgroundColor: "blue", color: "white" };
   return (
